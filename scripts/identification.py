d7657da78d3a84d396e97bb39e0f5d37d4ab68b1
sergiolopezsoriano <sergio.lopez@upf.edu>
2021-11-18 13:54:44 +0100

    pc detection ready for tests

---
+++ scripts/identification.py
@@ -0,0 +1,123 @@
+#!/usr/bin/env python
+
+import rospy
+from math import pi
+import traceback
+from ant_robot.msg import EpcInfo, ReadEpcsAction, ReadEpcsGoal
+from costmap_merge.srv import RobotIDs, RobotIDsResponse
+
+
+class Reading:
+    def __init__(self, antenna, rssi, stamp):
+        self.stamp = stamp
+        self.antenna_port = antenna
+        self.rssi = rssi
+
+
+class RobotTag:
+
+    def __init__(self, ID):
+        self.ID = ID
+        self.readings = list()
+
+    def add_reading(self, antenna, rssi, stamp):
+        self.readings.append(Reading(antenna, rssi, stamp))
+
+    def get_robot_data(self, antenna_map):
+        rssi = {'front': 0, 'left': 0, 'right': 0, 'back': 0}
+        total_rssi = 0
+        max_rssi = 0
+        for reading in self.readings:
+            # TODO: transform reading
+            rssi_lin = 10 ** (reading.rssi / 10)
+            total_rssi += 10 ** (reading.rssi / 10)
+            max_rssi = max(max_rssi, rssi_lin)
+            if reading.antenna_port == antenna_map['front']:
+                rssi['front'] += rssi_lin
+            elif reading.antenna_port == antenna_map['left']:
+                rssi['left'] += rssi_lin
+            elif reading.antenna_port == antenna_map['right']:
+                rssi['right'] += rssi_lin
+            elif reading.antenna_port == antenna_map['back']:
+                rssi['back'] += rssi_lin
+        angle = (2 * pi * rssi['front'] + pi / 2 * rssi['left'] + pi * rssi['back'] + 3 * pi / 2 * rssi['right']) / total_rssi
+        ''' sorted dictionary:       dict(sorted(rssi.items(), key=lambda item: item[1])) '''
+        return angle % (2 * pi), max_rssi
+
+
+class Identification:
+
+    def __init__(self):
+        self.tf_prefix = rospy.get_param('/tf_prefix', rospy.get_namespace()[1:-1] + '_tf/')
+        self.namespace = rospy.get_namespace().strip('/')
+        self.antenna_map = dict()
+        self.antenna_map['front'] = rospy.get_param('/' + self.namespace + '/rfid_interaction/prod/antenna_port/front')
+        self.antenna_map['back'] = rospy.get_param('/' + self.namespace + '/rfid_interaction/prod/antenna_port/back')
+        self.antenna_map['left'] = rospy.get_param('/' + self.namespace + '/rfid_interaction/prod/antenna_port/left')
+        self.antenna_map['right'] = rospy.get_param('/' + self.namespace + '/rfid_interaction/prod/antenna_port/right')
+        # Server controlling the main operation of discriminating and sorting a list of epcs
+        self.robot_epcs = list()
+        rospy.Service('/' + self.namespace + '/identification/robot_identification_srv', RobotIDs, self.cb_robot_identification)
+        # PART 1: check identification symmetry with other robot
+        # self.last_tags_pub = rospy.Publisher('/' + self.namespace + '/robot_ids', RobotList, queue_size=10)
+        self.robots = rospy.get_param('/' + self.namespace + '/identification/robots')
+        self.robot_ids = dict()
+        for robot in self.robots.items():
+            self.robot_ids[robot[1]['tag_id']] = robot[0]
+            self.robot_epcs.append(robot[1]['tag_id'])
+        self.rfid_detections = dict()
+
+    def empty_detections(self):
+        self.rfid_detections = dict()
+
+    def cb_robot_identification(self, req):
+        # PART 2: check identification symmetry with other robot
+        # rospy.Subscriber('/tags_update', String, self.cb_tags_update, queue_size=10)
+        self.empty_detections()
+        rfid_subs = rospy.Subscriber('/' + self.namespace + '/rfid_interaction/epcs', EpcInfo, self.cb_rfid_controller, queue_size=200)
+        while not rfid_subs.get_num_connections():
+            rospy.sleep(0.1)
+        rospy.sleep(2)
+        rfid_subs.unregister()
+        rssi = dict()
+        angle = dict()
+        for tag_id in self.rfid_detections:
+            angle[tag_id], rssi[tag_id] = self.rfid_detections[tag_id].get_robot_data(self.antenna_map)
+
+        sorted_detections = list()
+        for item in sorted(rssi.items(), key=lambda x: x[1]):
+            rospy.logdebug(item)
+            rospy.logdebug(angle[item[0]])
+            if pi / 3 >= angle[item[0]] or angle[item[0]] >= 5 * pi / 3:
+                sorted_detections.append(self.robot_ids[item[0]])
+
+        msg = RobotIDsResponse()
+        msg.sorted_detections = sorted_detections[0:req.num_detections]
+        msg.sorted_detections.reverse()
+        rospy.logdebug('[identification]: ' + str(msg.sorted_detections))
+        return msg
+
+    def cb_rfid_controller(self, msg):
+        if msg.epc in self.robot_epcs:
+            tag_id = msg.epc
+            antenna = msg.antenna_port
+            rssi = msg.RSSI
+            stamp = msg.ts
+            if tag_id not in self.rfid_detections:
+                self.rfid_detections[tag_id] = RobotTag(tag_id)
+            self.rfid_detections[tag_id].add_reading(antenna, rssi, stamp)
+            rospy.logdebug('[rfid reader]: ' + str(msg.epc) + ' : ' + str(msg.antenna_port) + ' : ' + str(msg.RSSI))
+
+
+if __name__ == "__main__":
+
+    try:
+        rospy.init_node('identification', log_level=rospy.INFO)
+        rospy.loginfo('[identification-' + rospy.get_namespace().strip('/') + ']: Node started')
+        identification = Identification()
+        rospy.spin()
+    except Exception as e:
+        rospy.logfatal('[identification-' + rospy.get_namespace().strip('/') + ']: Exception %s',
+                       str(e.message) + str(e.args))
+        e = traceback.format_exc()
+        rospy.logfatal(e)
--- scripts/pc_listener.py
+++ scripts/pc_listener.py
@@ -2,18 +2,18 @@
 
 import rospy
 import traceback
+from robots import OdomRobot
 import math
-from costmap_merge.srv import PCHandshake1, PCHandshake1Request, RobotIDs
-from helpers import PoseHelper
+from costmap_merge.srv import Handshake1, Handshake2, Handshake1Response, RobotIDs
+from helpers import TransformHelper, PoseHelper
 from visualization_msgs.msg import MarkerArray
 from nav_msgs.msg import Odometry
 
 
-class PCDetector:
+class PCListener:
     def __init__(self):
         # Getting ROS parameters
         self.namespace = rospy.get_namespace().strip('/')
-        self.tf_prefix = rospy.get_param('/' + self.namespace + '/tf_prefix')
         self.sim = rospy.get_param('/use_sim_time')
         if self.sim:
             self.robots_names = rospy.get_param('/robots_names')
@@ -23,10 +23,12 @@ class PCDetector:
         self.odom = Odometry()
         self.odom_ready = False
         rospy.Subscriber('/' + self.namespace + '/odom', Odometry, self.cb_odom, queue_size=1)
-        # Initializes the proxies dictionary to communicate with the detection_handshake node
-        self.handshake1_proxies = dict()
-        # List with the detected robots
-        self.detected_robots = list()
+        while not self.odom_ready:
+            rospy.sleep(1)
+        # Service to receive the robot poses in the detector's frame
+        rospy.Service('handshake1_service', Handshake1, self.cb_handshake1)
+        # Initializes the proxies dictionary to communicate with the detection_manager node
+        self.handshake2_proxies = dict()
         # Subscriber to the darknet topic publishing the location of detected robots
         rospy.Subscriber('/' + self.namespace + '/darknet_ros_3d/markers', MarkerArray, self.cb_detection, queue_size=1)
         self.marker_array = MarkerArray()
@@ -43,11 +45,20 @@ class PCDetector:
     def cb_detection(self, msg):
         self.marker_array = msg
 
+    def cb_handshake1(self, msg):
+        pose_D_R = self.rfid_detection(msg.detector_ns)
+        pose_R_R = PoseHelper.get_pose_from_odom(self.odom)
+        beta = math.atan2(pose_D_R.pose.position.y - pose_R_R.pose.position.y,
+                          pose_D_R.pose.position.x - pose_R_R.pose.position.x)
+        if pose_D_R:
+            self.handshake2(msg.detector_ns, msg.pose_R_D, pose_R_R, msg.alpha, beta)
+        return Handshake1Response()
+
     @staticmethod
     def get_distance_to_detection(marker):
         return math.sqrt(marker.pose.position.x ** 2 + marker.pose.position.y ** 2)
 
-    def rfid_detection(self):
+    def rfid_detection(self, detector):
         distance = list()
         for marker in self.marker_array.markers:
             distance.append(self.get_distance_to_detection(marker))
@@ -56,49 +67,37 @@ class PCDetector:
         sorted_robots = self.detection_check_proxy(len(sorted_indexes))
         count = 0
         for robot in sorted_robots:
-            if robot not in self.detected_robots:
-                self.detected_robots.append(robot)
-            else:
-                self.detected_robots.remove(robot)
-            self.locate_robot(robot, count)
+            if robot == detector:
+                return self.get_pose_D_R(count)
             count += 1
 
-    def locate_robot(self, robot, count):
-        pose_D_D = PoseHelper.get_pose_from_odom(self.odom)
-        pose_R_D = self.get_pose_R_D(count)
-        alpha = math.atan2(pose_R_D.pose.position.y - pose_D_D.pose.position.y,
-                           pose_R_D.pose.position.x - pose_D_D.pose.position.x)
-        self.handshake1(robot, pose_D_D, pose_R_D, alpha)
-
-    def get_pose_R_D(self, count):
+    def get_pose_D_R(self, count):
         x = self.marker_array.markers[count].pose.position.x + self.odom.pose.pose.position.x
         y = self.marker_array.markers[count].pose.position.y + self.odom.pose.pose.position.y
-        pose_R_D = PoseHelper.set_2D_pose(self.tf_prefix + '_tf/odom', rospy.Time.now(), [x, y, 0, 0, 0, 0])
-        return pose_R_D
+        pose_D_R = PoseHelper.set_2D_pose(self.namespace + '_tf/odom', rospy.Time.now(), [x, y, 0, 0, 0, 0])
+        return pose_D_R
 
-    def handshake1(self, robot, pose_D_D, pose_R_D, alpha):
-        """ Sends the detector and robot poses in the detector frame to the detected robot """
+    def handshake2(self, detector_ns, pose_R_D, pose_R_R, alpha, beta):
+        """ Sends the detector and robot poses in the detector frame to the detection_manager node in the Detector """
+        pose_R_D = TransformHelper.get_pose_orientation(pose_R_D, pose_R_R, alpha, beta)
         try:
-            self.handshake1_proxies[robot](self.namespace, pose_D_D, pose_R_D, alpha)
+            self.handshake2_proxies[detector_ns](self.namespace, pose_R_D, pose_R_R, alpha, beta)
         except KeyError:
-            rospy.wait_for_service('/' + robot + '/handshake1_service')
-            self.handshake1_proxies[robot] = rospy.ServiceProxy('/' + robot + '/handshake1_service', PCHandshake1)
-            self.handshake1_proxies[robot](self.namespace, pose_D_D, pose_R_D, alpha)
+            rospy.wait_for_service('/' + detector_ns + '/handshake2_service')
+            self.handshake2_proxies[detector_ns] = rospy.ServiceProxy('/' + detector_ns + '/handshake2_service',
+                                                                      Handshake2)
+            self.handshake2_proxies[detector_ns](self.namespace, pose_R_D, pose_R_R, alpha, beta)
 
 
 if __name__ == "__main__":
 
     try:
-        rospy.init_node('pc_detector', log_level=rospy.INFO)
-        rospy.logdebug('[pc_detector]: Node started')
-        pc_detector = PCDetector()
-        while not pc_detector.odom_ready:
-            rospy.sleep(1)
-        while not rospy.is_shutdown():
-            pc_detector.rfid_detection()
-            rospy.sleep(1)
+        rospy.init_node('pc_listener', log_level=rospy.INFO)
+        rospy.logdebug('[pc_listener]: Node started')
+        pc_listener = PCListener()
+        rospy.spin()
 
     except Exception as e:
-        rospy.logfatal('[pc_detector]: Exception %s', str(e.message) + str(e.args))
+        rospy.logfatal('[pc_listener]: Exception %s', str(e.message) + str(e.args))
         e = traceback.format_exc()
         rospy.logfatal(e)
